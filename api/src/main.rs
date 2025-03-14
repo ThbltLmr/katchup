9b7d2a09476091c288cc92feda473dfd96fd7d9e
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-03-14 15:38:16 +0100

    base server

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,11 +1,29 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_stream(mut stream: TcpStream) {
+    let reader = BufReader::new(&stream);
+    let http_request: Vec<_> = reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
-    
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!('Hello there');
+        handle_stream(stream);
     }
 }
