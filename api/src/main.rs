32fe4bc1751624ea5747cc9a9891d7e10f4e23a2
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-03-15 22:12:19 +0100

    create constants

--- api/src/main.rs
+++ api/src/main.rs
@@ -5,6 +5,9 @@ use std::{
 
 use api::ThreadPool;
 
+const PORT_NUMBER: &str = "8000";
+const IP: &str = "127.0.0.1";
+
 fn handle_stream(mut stream: TcpStream) {
     let reader = BufReader::new(&stream);
     let http_request: Vec<_> = reader
@@ -13,7 +16,13 @@ fn handle_stream(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {http_request:#?}");
+    println!("Request received: {http_request:#?}");
+    let request_line = reader.lines().next().unwrap().unwrap();
+
+    let (Some(method), Some(url), Some(version), None) = request_line.split_whitespace() else {
+        println!("Unexpected request format");
+        return;
+    }
 
     let response = "HTTP/1.1 200 OK\r\n\r\n";
 
@@ -21,7 +30,7 @@ fn handle_stream(mut stream: TcpStream) {
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    let listener = TcpListener::bind(format!("{}:{}", IP, PORT_NUMBER)).unwrap();
     let pool = ThreadPool::build(4).expect("Could not build thread pool");
 
     for stream in listener.incoming() {
