f2362aa3ba05da50a220c51cb4e5049b2b965633
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-03-27 08:39:42 +0100

    feat: Implement get_tv_show function in TmdbAdapter

--- api/src/tmdb_adapter.rs
+++ api/src/tmdb_adapter.rs
@@ -1,5 +1,6 @@
 use reqwest::{Method, blocking::Client, header::ACCEPT};
 use serde::{Deserialize, Serialize};
+use std::error::Error;
 
 pub struct TmdbAdapter {
     client: Client,
@@ -17,6 +18,12 @@ pub struct SearchResults {
     pub results: Vec<TvResult>,
 }
 
+#[derive(Deserialize, Serialize)]
+pub struct ShowDetails {
+    pub number_of_episodes: u8,
+    pub number_of_seasons: u8,
+}
+
 impl TmdbAdapter {
     pub fn new() -> Self {
         Self {
@@ -24,7 +31,7 @@ impl TmdbAdapter {
         }
     }
 
-    pub fn search_tv_show(&self, query: &str) -> Result<SearchResults, Box<dyn std::error::Error>> {
+    pub fn search_tv_show(&self, query: &str) -> Result<SearchResults, Box<dyn Error>> {
         let request_url = format!("https://api.themoviedb.org/3/search/tv?{}", query);
 
         let api_token = std::env::var("TMDB_API_TOKEN").unwrap();
@@ -37,7 +44,28 @@ impl TmdbAdapter {
 
         let response = builder.send()?;
 
-        println!("TMDB respnse: {response:#?}");
+        println!("TMDB response: {response:#?}");
+
+        Ok(response.json().expect("Could not format json"))
+    }
+
+    pub fn get_tv_show(&self, query: &str) -> Result<ShowDetails, Box<dyn Error>> {
+        let request_url = format!(
+            "https://api.themoviedb.org/3/search/tv/{}",
+            query.split_once('=').expect("Could not find id in query").0
+        );
+
+        let api_token = std::env::var("TMDB_API_TOKEN").unwrap();
+
+        let builder = self
+            .client
+            .request(Method::GET, request_url)
+            .bearer_auth(api_token)
+            .header(ACCEPT, "application/json");
+
+        let response = builder.send()?;
+
+        println!("TMDB response: {response:#?}");
 
         Ok(response.json().expect("Could not format json"))
     }
