bbf38d480ab1727b06727e01905bb26dfaa32713
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-03-28 16:46:00 +0100

    feat(ollama): add ollama adapter for summarizing shows
    This commit introduces an Ollama adapter to summarize TV shows based on a given query. The adapter utilizes the Ollama API to generate summaries and prints the response.

--- api/src/adapters/ollama_adapter.rs
+++ api/src/adapters/ollama_adapter.rs
@@ -1,7 +1,15 @@
-use reqwest::{blocking::Client, header::ACCEPT, Method};
+use reqwest::blocking::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 pub struct OllamaAdapter {
     client: Client,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct SummaryResult {
+    pub done: bool,
+    pub response: String,
 }
 
 impl OllamaAdapter {
@@ -9,5 +17,28 @@ impl OllamaAdapter {
         Self {
             client: Client::new(),
         }
+    }
+
+    pub fn summarize_show(&self, query: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let show = query
+            .split_once('=')
+            .expect("Could not find show name in query")
+            .1;
+
+        let url = format!("{}/api/generate", std::env::var("OLLAMA_API_URL").unwrap());
+
+        let prompt = format!("Tell me a summary about the TV show: {}", show);
+
+        let body = json!({
+            "model": "llama3.2:3b",
+            "stream": false,
+            "prompt": prompt
+        });
+
+        let response = self.client.post(url).body(body.to_string()).send().unwrap();
+
+        println!("{:?}", response);
+
+        Ok(())
     }
 }
