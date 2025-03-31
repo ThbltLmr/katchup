3948a1a8826ba26475ea635e60c287c0e2e8a9b8
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-03-31 12:07:04 +0200

    feat(server): implement HttpResponseBuilder::build_or_default_to_500
    This commit introduces the  method to the .
    This method constructs an  based on the provided code and body. If the code
    is not found in the http_code_map, it defaults to a 500 Internal Server Error.
    
    The commit also includes changes to the  method to accept a code tuple
    and a body, and adds a  method to construct a default 500 error response.

--- api/src/server/server.rs
+++ api/src/server/server.rs
@@ -17,7 +17,16 @@ impl HttpResponseBuilder {
         HttpResponseBuilder { http_code_map }
     }
 
-    pub fn build_or_default_to_500(code: u16, body: Value) -> HttpResponse {}
+    pub fn build_or_default_to_500(&self, code: u16, body: Value) -> HttpResponse {
+        match self.http_code_map.get(&code) {
+            Some(message) => HttpResponse {
+                code: (code, message),
+                body,
+                format_string: S,
+            },
+            None => self.format_response((500, "Internal Server Error"), body),
+        }
+    }
 
     fn create_http_code() -> HashMap<u16, String> {
         let mut map = HashMap::new();
@@ -29,18 +38,30 @@ impl HttpResponseBuilder {
         map
     }
 
-    fn format_response(&self, response: HttpResponse) -> String {
-        match serde_json::to_string(&response.body) {
+    fn format_response(&self, code: (u16, &str), body: Value) -> String {
+        match serde_json::to_string(&body) {
             Ok(formatted_body) => {
                 format!(
                     "HTTP/1.1 {} {}\r\ncontent-type: application/json\r\ncontent-length: {}\r\nAccess-Control-Allow-Headers: *\r\nAccess-Control-Allow-Methods: *\r\norigin: *\r\nAccess-Control-Allow-Origin: http://localhost:5173\r\n\r\n{}",
-                    response.code.0,
-                    response.code.1,
+                    code.0,
+                    code.1,
                     formatted_body.len(),
                     formatted_body,
                 )
             }
             Err(_) => "failed to serialize response body".to_string(),
+        }
+    }
+
+    fn build_500(&self) -> HttpResponse {
+        let code: u16 = 500;
+        let message: String = self.http_code_map.get(&code).unwrap().to_string();
+        let body = serde_json::Value::String("".to_string());
+
+        HttpResponse {
+            code: (code, message.clone()),
+            body: body.clone(),
+            format_string: self.format_response((code, &message), body),
         }
     }
 }
