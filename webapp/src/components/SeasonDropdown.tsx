b54ea07f0dbbb4f2d7017e6ef7cf8bd858f53bba
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-04-01 19:21:22 +0200

    feat(webapp): use ui/select component for SeasonDropdown
    The SeasonDropdown component was refactored to use the ui/select component instead of the ui/command component. This provides a better user experience.

--- webapp/src/components/SeasonDropdown.tsx
+++ webapp/src/components/SeasonDropdown.tsx
@@ -1,58 +1,45 @@
-
-import { Check } from "lucide-react"
-
-import { cn } from "@/lib/utils"
 import {
-  Command,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command"
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import { useGetShow, SeasonResult } from "@/hooks/useGetShow";
-import { useState } from "react";
 
-function SeasonAndEpisodeDropdown({ showId, setSelectedSeason, season }: { showId: number, season: SeasonResult | undefined, setSelectedSeason: React.Dispatch<React.SetStateAction<SeasonResult | undefined>> }) {
-  const [seasonValue, setSeasonValue] = useState("")
-  const [open, setOpen] = useState(true);
-
+function SeasonDropdown({ showId, setSelectedSeason, season }: { showId: number, season: SeasonResult | undefined, setSelectedSeason: React.Dispatch<React.SetStateAction<SeasonResult | undefined>> }) {
   const { data } = useGetShow(showId);
 
   const seasons = data?.ShowDetails.seasons || [];
 
   const handleSelectSeasonValue = (value: number) => {
-    setSeasonValue(value.toString());
     setSelectedSeason(seasons.find((season) => season.id === value));
-    setOpen(false);
   }
 
 
   return (
-    <Command className={`m-4 transition-all h-full duration-300 ease-in-out ${!!season ? 'w-1/4' : 'w-1/2'}`}>
-      <CommandList>
-        <CommandInput placeholder="Choose a season" onClick={() => setOpen(true)} value={!!season ? `Season ${(seasons.findIndex((season) => season.id === season.id)! + 1).toString()}` : undefined} />
-        <CommandGroup hidden={!open}>
-          {seasons.map((season, i) => (
-            <CommandItem
-              key={season.id}
-              value={season.id.toString()}
-              onSelect={(currentValue) => {
-                handleSelectSeasonValue(currentValue === seasonValue ? 0 : season.id)
-              }}
-            >
-              <Check
-                className={cn(
-                  "mr-2 h-4 w-4",
-                  seasonValue === season.id.toString() ? "opacity-100" : "opacity-0"
-                )}
-              />
-              Season {i + 1}
-            </CommandItem>
-          ))}
-        </CommandGroup>
-      </CommandList>
-    </Command>
+    <div className={`m-4 transition-all h-full duration-300 ease-in-out ${!!season ? 'w-1/4' : 'w-1/2'}`}>
+      <Select onValueChange={(currentValue) => {
+        handleSelectSeasonValue(parseInt(currentValue))
+      }}>
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder="Select a season" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {seasons.map((season, i) => (
+              <SelectItem
+                value={season.id.toString()}
+              >
+                Season {i + 1}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
   )
 }
 
-export default SeasonAndEpisodeDropdown
+export default SeasonDropdown
