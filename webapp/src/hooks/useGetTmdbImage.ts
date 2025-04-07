43c6f075ee589d493a2c44c58e45b40b8982634c
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-04-07 11:35:12 +0200

    feat(image): add hook to construct TMDB image URLs
    This commit introduces a new hook, `useGetTmdbImage`, that generates URLs for images from the TMDB API. It also defines the `PosterSizes` and `ProfileSizes` enums.

---
+++ webapp/src/hooks/useGetTmdbImage.ts
@@ -0,0 +1,24 @@
+enum PosterSizes {
+	w92 = "w92",
+	w154 = "w154",
+	w185 = "w185",
+	w342 = "w342",
+	w500 = "w500",
+	w780 = "w780",
+	original = "original"
+}
+
+enum ProfileSizes {
+	w45 = "w45",
+	w185 = "w185",
+	h632 = "h632",
+	original = "original"
+}
+
+const useGetTmdbImage = (filePath: string, fileSize: ProfileSizes | PosterSizes): string =>
+	`${import.meta.env.VITE_TMDB_IMAGE_URL}/${fileSize}/${filePath}`
+	;
+
+export default useGetTmdbImage;
+export { PosterSizes, ProfileSizes }
+
