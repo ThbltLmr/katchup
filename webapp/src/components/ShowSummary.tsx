62a266e2024012d2ac3bdb4050b1cf7cec442313
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-04-11 11:29:34 +0200

    feat: add ShowSummary component\n\nThis component fetches and displays the summary of a TV show episode, given the show name, season number, and episode number. It uses the useGetSummary hook to fetch the data and displays a loading message while the data is being fetched.

---
+++ webapp/src/components/ShowSummary.tsx
@@ -0,0 +1,11 @@
+import useGetSummary from "@/hooks/useGetSummary";
+
+function ShowSummary({ show, season, episode }: { show: string, season: number, episode: number }) {
+  const { data, isLoading } = useGetSummary(show, season, episode);
+
+  return (
+    <>
+      {isLoading ? <p>Loading...</p> : <p>{data?.SummaryResult.response}</p>}
+    </>
+  )
+}
