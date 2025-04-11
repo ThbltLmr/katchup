57b2c36e3442ce2288b20ce69acb68608d45d07c
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-04-11 10:54:59 +0200

    feat: display cast information with loading state
    This commit introduces the following changes:
    
    - Displays the cast information for a TV show, including the character name, actor name, character description, and profile image.
    - Implements a loading state using skeleton components to improve the user experience while the cast data is being fetched.
    - Adjusts the layout and styling of the cast cards to enhance visual appeal and readability.

--- webapp/src/components/CastList.tsx
+++ webapp/src/components/CastList.tsx
@@ -8,42 +8,59 @@ import {
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { useGetCast } from "@/hooks/useGetCast";
+import { CastResult, useGetCast } from "@/hooks/useGetCast";
 import useGetTmdbImage, { ProfileSizes } from "@/hooks/useGetTmdbImage";
+import { Skeleton } from "./ui/skeleton";
 
 function CastList({ showId }: { showId: number }) {
-  const { data } = useGetCast(showId);
+  const { data, isError, isLoading } = useGetCast(showId);
 
   const cast = data?.CastDetails.cast || [];
+
+  const castCards = (cast: CastResult[]) => {
+    return (
+      cast.map((actor) =>
+        <CarouselItem key={actor.id} className="basis-1/3 w-1/4">
+          <Card className="h-[400px]">
+            <CardContent className="center">
+              {!!actor.profile_path &&
+                <img className="h-[250px] w-full rounded-xl object-cover" src={useGetTmdbImage(actor.profile_path, ProfileSizes.original)} />
+              }
+              <div className="py-4 text-start">
+                <span className="h-4 text-xl w-full font-bold">{actor.roles[0].character}</span>
+                <span className="h-4 text-base w-full"> ({actor.name})</span>
+                <p className="h-4 pt-4 text-start text-sm w-full">{(!!actor.character_description && actor.character_description.length > 0) ? actor.character_description : "No description available"}</p>
+              </div>
+            </CardContent>
+            <CardFooter>
+            </CardFooter>
+          </Card>
+
+        </CarouselItem>
+      ))
+  }
+
+  const castSkeleton = () => {
+    return (
+      <div className="flex flex-col space-y-3">
+        <Skeleton className="h-[250px] w-[250px] rounded-xl" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+    )
+  }
 
   return (
     <Carousel>
       <CarouselContent className="p-4">
-        {cast.map((actor) =>
-          <CarouselItem key={actor.id} className="basis-1/4 w-1/5">
-            <Card>
-              <CardHeader>
-                <CardTitle className="text-base">{actor.roles[0].character}</CardTitle>
-              </CardHeader>
-              <CardContent className="center">
-                {!!actor.character_description &&
-                  <p>{actor.character_description}</p>
-                }
-                {!!actor.profile_path &&
-                  <img className="mx-auto object-scale-down" src={useGetTmdbImage(actor.profile_path, ProfileSizes.w185)} />
-                }
-              </CardContent>
-              <CardFooter>
-                <p className="text-center w-full text-sm">Played by {actor.name}</p>
-              </CardFooter>
-            </Card>
-
-          </CarouselItem>
-        )}
+        {isLoading ? castSkeleton() : castCards(cast)}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
