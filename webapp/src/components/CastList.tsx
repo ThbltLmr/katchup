0a1521284a650154015b537c921640e8e7e3387d
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-04-15 12:30:43 +0200

    feat: Adjust cast list layout for improved display\n\nThe cast list layout has been updated to display more cast members in the carousel. The size of each cast card and image has been reduced to accommodate the increased number of items. Additionally, padding has been reduced.

--- webapp/src/components/CastList.tsx
+++ webapp/src/components/CastList.tsx
@@ -22,15 +22,15 @@ function CastList({ showId }: { showId: number }) {
   const castCards = (cast: CastResult[]) => {
     return (
       cast.map((actor) =>
-        <CarouselItem key={actor.id} className="basis-1/3 w-1/4">
-          <Card className="h-[400px]">
+        <CarouselItem key={actor.id} className="basis-1/5 w-1/6">
+          <Card className="h-[300px]">
             <CardContent className="center">
               {!!actor.profile_path &&
-                <img className="h-[250px] w-full rounded-xl object-cover" src={useGetTmdbImage(actor.profile_path, ProfileSizes.original)} />
+                <img className="h-[150px] w-full rounded-xl object-cover" src={useGetTmdbImage(actor.profile_path, ProfileSizes.w185)} />
               }
-              <div className="py-4 text-start">
-                <span className="h-4 text-xl w-full font-bold">{actor.roles[0].character}</span>
-                <span className="h-4 text-base w-full"> ({actor.name})</span>
+              <div className="py-2 text-start">
+                <span className="h-4 text-base w-full font-bold">{actor.roles[0].character}</span>
+                <span className="h-4 text-sm w-full"> ({actor.name})</span>
                 <p className="h-4 pt-4 text-start text-sm w-full">{(!!actor.character_description && actor.character_description.length > 0) ? actor.character_description : "No description available"}</p>
               </div>
             </CardContent>
@@ -45,12 +45,12 @@ function CastList({ showId }: { showId: number }) {
   const castSkeleton = () => {
     return (
       <div className="flex justify-between w-full">
-        {Array.from({ length: 3 }).map(() =>
-          <div className="flex flex-col h-[400px] basis-1/3 p-4 w-1/4">
-            <Skeleton className="h-[250px] w-full rounded-xl" />
+        {Array.from({ length: 5 }).map(() =>
+          <div className="flex flex-col h-[400px] basis-1/5 p-4 w-1/6">
+            <Skeleton className="h-[150px] w-full rounded-xl" />
             <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="h-4 w-full" />
             </div>
           </div>
         )}
@@ -60,7 +60,7 @@ function CastList({ showId }: { showId: number }) {
 
   return (
     <Carousel>
-      <CarouselContent className="p-4">
+      <CarouselContent className="p-2">
         {isLoading ? castSkeleton() : castCards(cast)}
       </CarouselContent>
       <CarouselPrevious />
