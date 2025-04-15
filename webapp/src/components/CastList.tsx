ec4261cd8a0ff0c465d70b887749ff035c377af5
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-04-15 20:39:26 +0200

    fix(CastList): improve formatting of cast cards

--- webapp/src/components/CastList.tsx
+++ webapp/src/components/CastList.tsx
@@ -23,15 +23,15 @@ function CastList({ showId }: { showId: number }) {
     return (
       cast.map((actor) =>
         <CarouselItem key={actor.id} className="basis-1/5 w-1/6">
-          <Card className="h-[300px]">
-            <CardContent className="center">
-              {!!actor.profile_path &&
-                <img className="h-[120px] w-full rounded-xl object-cover" src={useGetTmdbImage(actor.profile_path, ProfileSizes.w185)} />
-              }
-              <div className="py-2 text-start">
-                <span className="h-4 text-sm w-full font-bold">{actor.roles[0].character}</span>
-                <span className="h-4 text-xs w-full"> ({actor.name})</span>
-                <p className="h-4 pt-4 text-start text-xs w-full">{(!!actor.character_description && actor.character_description.length > 0) ? actor.character_description : "No description available"}</p>
+          <Card className="center h-[300px]">
+            <CardContent className="h-full">
+              <div className="flex flex-col h-full justify-between">
+                {!!actor.profile_path &&
+                  <img className="h-[120px] w-full rounded-xl object-cover" src={useGetTmdbImage(actor.profile_path, ProfileSizes.w185)} />
+                }
+                <p className="h-4 text-sm w-full font-bold">{actor.roles[0].character}</p>
+                <p className="h-4 text-xs w-full"> ({actor.name})</p>
+                <p className="h-20 text-start align-middle text-xs w-full">{(!!actor.character_description && actor.character_description.length > 0) ? actor.character_description : "No description available"}</p>
               </div>
             </CardContent>
             <CardFooter>
