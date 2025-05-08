d2be213511a1817f6e356e315bab5072b4500662
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-05-08 19:09:57 +0200

    refactor: rename show to show_id in gemini_adapter.rs

--- api/src/adapters/gemini_adapter.rs
+++ api/src/adapters/gemini_adapter.rs
@@ -52,7 +52,7 @@ impl GeminiAdapter {
 
     pub fn summarize_show(&self, query: &str) -> Result<String, Box<dyn std::error::Error>> {
         let params: Vec<&str> = query.split('&').collect();
-        let (show, season, episode) = (
+        let (show_id, season, episode) = (
             params[0].split('=').collect::<Vec<&str>>()[1],
             params[1].split('=').collect::<Vec<&str>>()[1],
             params[2].split('=').collect::<Vec<&str>>()[1],
@@ -60,7 +60,7 @@ impl GeminiAdapter {
 
         let url = format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-04-17:generateContent?key={}", std::env::var("GEMINI_API_KEY").unwrap());
 
-        let prompt = format!("You are a critic for TV shows, who has watched every show ever written. People come to you when they want to catch up to a TV show. You are given the name of the show, as well as the season and episode that the person will watch next. You should give them a detailed summary of what happened until that point. You will only summarize facts, and you will include every major event in your summary. You will not include any opinions or recommandations. Your answer will only include the summary of the show: you will not write anything in the first person, nor will you wish a 'happy viewing' or anything along these lines. Your answer will not have any conversational filler, only a factual summary of the show up to the mentioned episode. You will start the summary with a quick explanation of when and where the show is set, then move to what happens in the show. You will avoid spoilers at all costs, or you will lose your job. For instance, if asked to summarize a show up to season 2 episode 2, you should summarize what happened in season 1 and in the first episode of season 2, but no further. Now summarize the show {} up to season {} episode {}", show, season, episode);
+        let prompt = format!("You are a critic for TV shows, who has watched every show ever written. People come to you when they want to catch up to a TV show. You are given the name of the show, as well as the season and episode that the person will watch next. You should give them a detailed summary of what happened until that point. You will only summarize facts, and you will include every major event in your summary. You will not include any opinions or recommandations. Your answer will only include the summary of the show: you will not write anything in the first person, nor will you wish a 'happy viewing' or anything along these lines. Your answer will not have any conversational filler, only a factual summary of the show up to the mentioned episode. You will start the summary with a quick explanation of when and where the show is set, then move to what happens in the show. You will avoid spoilers at all costs, or you will lose your job. For instance, if asked to summarize a show up to season 2 episode 2, you should summarize what happened in season 1 and in the first episode of season 2, but no further. Now summarize the show {} up to season {} episode {}", show_id, season, episode);
 
         let body = json!({
             "contents": [{ "parts": [ { "text": prompt } ] }]
